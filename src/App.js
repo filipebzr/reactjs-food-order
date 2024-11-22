16197a3d70a668a33be579e01df13485b323c8d9
FilipeBzr <fil@pe.com>
2024-11-22 19:26:38 -0300

    added cart modal

--- src/App.js
+++ src/App.js
@@ -4,21 +4,20 @@ import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart';
 
 function App() {
   const [cartIsShown, setCartIsShow] = useState(false);
-
   function showCartHandle(){
     setCartIsShow(true);
   }
   function hideCartHandle(){
     setCartIsShow(false);
-
   }
-
   return (
     <CartProvider>
       {cartIsShown && <div>Cart...</div>}
+      <Cart/>
       <Header onShowCart={showCartHandle}/>
       <main>
         <Meals />
